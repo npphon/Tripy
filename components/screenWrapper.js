99fcdeeb799a1233df6597ff3c53771d2a1e6cbb
Nattawat Isanpayu <nattawat@MBPkhxnNattawat.lan>
2023-12-04 22:21:19 +0700

    #3

--- components/screenWrapper.js
+++ components/screenWrapper.js
@@ -2,7 +2,7 @@ import { View, Text, StatusBar, Platform } from 'react-native'
 import React from 'react'
 
 export default function screenWrapper({children}) {
-    let StatusBarHeight = StatusBar.currentHeight? StatusBar.currentHeight: Platform.OS=='ios'? 30: 0;;
+    let StatusBarHeight = StatusBar.currentHeight? StatusBar.currentHeight: Platform.OS=='ios'? 30: 0;
   return (
     <View style={{paddingTop: StatusBarHeight}}>
         {
