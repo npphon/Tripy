e6be09ddfa8cb0cbb148eff1221986df884ca84e
Nattawat Isanpayu <nattawat@nattawat.lan>
2023-12-22 21:44:51 +0700

    #4

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,80 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '../components/screenWrapper'
+import { colors } from '../theme'
+import randomImage from '../assets/images/randomImage';
+import EmptyLost from '../components/emptyList';
+const items = [
+    {
+      id: 1,
+      place: 'Gujrat',
+      country: 'pakistan',
+    },
+    {
+
+      id: 2,
+      place: 'London Eye',
+      country: 'England',
+    },
+    {
+      id: 3,
+      place: 'Washington dc',
+      country: 'America',
+    },
+    {
+      id: 4,
+      place: 'New york',
+      country: 'America',
+    },
+
+  ]
 
 export default function HomeScreen() {
   return (
-    <ScreenWrapper>
-      <Text>HomeScreen</Text>
+    <ScreenWrapper className="flex-1">
+      <View className="flex-row justify-between items-center p-4">
+        <Text className={`${colors.heading} font-bold text-3xl shadow-sm`}>Expensify</Text>
+        <TouchableOpacity className="p-2 px-3 bg-white border border-gray-200 rounded-full">
+          <Text className={colors.heading}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+      <View className="flex justify-center items-center bg-blue-200 rounded-xl mx-4 mb-4">
+        <Image className="w-60 h-60" source={require('../assets/images/banner.png')} />
+      </View>
+      <View className="px-4 space-y-4">
+        <View className="flex-row justify-between items-center">
+        <Text className={`${colors.heading} font-bold text-xl `}>Recent Trips</Text>
+        <TouchableOpacity className="p-2 px-3 bg-white border border-gray-200 rounded-full">
+          <Text className={colors.heading}>Add Trip</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={{heading: 430}}>
+          <FlatList 
+          data={[]}
+          numColumns={2}
+          ListEmptyComponent={<EmptyLost /> }
+          keyExtractor={item=> item.id}
+          showsHorizontalScrollIndicator={false}
+          columnWrapperStyle={{
+              justifyContent: 'space-between'
+            }}
+            className="mx-1"
+          renderItem={({item})=>{
+            return(
+              <TouchableOpacity className=" bg-white p-3  rounded-2xl mb-3 shadow-sm">
+                <View>
+                  <Image  className="w-36 h-36 mb-2" source={randomImage()}  />
+                  <Text className={`${colors.heading} font-bold `}>{item.place}</Text>
+                  <Text className={`${colors.heading}`}>{item.country}</Text>
+                </View>
+              </TouchableOpacity>
+            )
+            
+          }}
+          />
+
+        </View>
+      </View>
     </ScreenWrapper>
   )
 }
---
+++ theme/index.js
@@ -0,0 +1,6 @@
+import { Button } from "react-native";
+
+export const colors = {
+    heading: "text-gray-700",
+    Button:'#50C878'
+}
